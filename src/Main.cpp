37dc81646c3ab5b9c5e32bf68e437f33fd3dcccb
Kevin Balz <kevin.balz@web.de>
2019-10-05 15:49:42 +0200

    Camera

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,19 +1,33 @@
 #include "Tako.hpp"
 
+namespace {
+    constexpr float gravity = 16;
+}
 struct State
 {
     tako::Vector2 position;
+    tako::Vector2 cameraPosition;
 } state;
 
 void tako::Setup(PixelArtDrawer* drawer)
 {
+    state.position = Vector2(0, 30);
+    state.cameraPosition = state.position;
+    drawer->SetClearColor({"#87CEEB"});
+    drawer->SetTargetSize(240, 135);
+    drawer->AutoScale();
+    drawer->SetCameraPosition({0, 0});
 }
 
 void tako::Update(tako::Input* input, float dt)
 {
-    const float speed = 10;
+    const float speed = 16;
     tako::Vector2 movement;
     if (input->GetKey(tako::Key::W))
+    {
+        movement.y++;
+    }
+    if (input->GetKey(tako::Key::S))
     {
         movement.y--;
     }
@@ -21,21 +35,21 @@ void tako::Update(tako::Input* input, float dt)
     {
         movement.x--;
     }
-    if (input->GetKey(tako::Key::S))
-    {
-        movement.y++;
-    }
     if (input->GetKey(tako::Key::D))
     {
         movement.x++;
     }
     //movement.normalize();
     state.position += movement * speed * dt;
+    //state.position.y -= gravity * dt;
+    state.cameraPosition += (state.position + Vector2(8, -8) - state.cameraPosition) * dt;
 }
 
 void tako::Draw(PixelArtDrawer* drawer)
 {
     drawer->Clear();
-    drawer->DrawRectangle(state.position.x, state.position.y, 100, 100, {100, 0, 255, 255});
+    drawer->SetCameraPosition(state.cameraPosition);
+    drawer->DrawRectangle(-1000, 0, 2000, 2000, {"#B5651D"});
+    drawer->DrawRectangle(state.position.x, state.position.y, 16, 16, {100, 0, 255, 255});
 }
 
