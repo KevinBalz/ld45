67cc6c3109aaf58194115e2a18993eb5be0c8599
Kevin Balz <kevin.balz@web.de>
2019-10-05 18:33:53 +0200

    World bounds

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,17 +2,20 @@
 
 namespace {
     constexpr float gravity = 16;
+    constexpr float worldLeft = -256;
+    constexpr float worldRight = 256;
 }
 struct State
 {
     tako::Vector2 position;
     tako::Vector2 cameraPosition;
+    tako::Vector2 cameraTarget;
 } state;
 
 void tako::Setup(PixelArtDrawer* drawer)
 {
     state.position = Vector2(0, 30);
-    state.cameraPosition = state.position;
+    state.cameraPosition = state.cameraTarget = state.position + Vector2(8, -8);
     drawer->SetClearColor({"#87CEEB"});
     drawer->SetTargetSize(240, 135);
     drawer->AutoScale();
@@ -21,7 +24,7 @@ void tako::Setup(PixelArtDrawer* drawer)
 
 void tako::Update(tako::Input* input, float dt)
 {
-    const float speed = 16;
+    const float speed = 32;
     tako::Vector2 movement;
     if (input->GetKey(tako::Key::W))
     {
@@ -41,8 +44,15 @@ void tako::Update(tako::Input* input, float dt)
     }
     //movement.normalize();
     state.position += movement * speed * dt;
+    state.position.x = std::max(worldLeft, std::min(state.position.x, worldRight + 32));
     //state.position.y -= gravity * dt;
-    state.cameraPosition += (state.position + Vector2(8, -8) - state.cameraPosition) * dt;
+    if (tako::mathf::abs(state.cameraTarget.x - state.position.x) > 32 || tako::mathf::abs(state.cameraTarget.x - state.cameraPosition.x) > 16)
+    {
+        auto extents = tako::Graphics->GetCameraViewSize() / 2;
+        state.cameraTarget.x = std::max(worldLeft + extents.x, std::min(state.position.x, worldRight - extents.x));
+    }
+
+    state.cameraPosition += (state.cameraTarget - state.cameraPosition) * dt;
 }
 
 void tako::Draw(PixelArtDrawer* drawer)
@@ -50,6 +60,7 @@ void tako::Draw(PixelArtDrawer* drawer)
     drawer->Clear();
     drawer->SetCameraPosition(state.cameraPosition);
     drawer->DrawRectangle(-1000, 0, 2000, 2000, {"#B5651D"});
+    drawer->DrawRectangle(-32, 8, 8, 8, {"#B5651D"});
     drawer->DrawRectangle(state.position.x, state.position.y, 16, 16, {100, 0, 255, 255});
 }
 
